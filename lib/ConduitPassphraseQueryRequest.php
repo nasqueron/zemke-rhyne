50dcb151bbf0511b8b821fe5aef7379446a29858
Sébastien Santoro <dereckson@espace-win.org>
2014-11-20 06:05:23 +0100

    Adding properties to ConduitPassphraseQueryRequest
    $ids, $needPublicKeys and $needSecrets where declared implicitly by the constructor
    
    This change adds them explicitly.

--- lib/ConduitPassphraseQueryRequest.php
+++ lib/ConduitPassphraseQueryRequest.php
@@ -3,6 +3,10 @@
 require 'ConduitRequest.php';
 
 class ConduitPassphraseQueryRequest extends ConduitRequest {
+	public $ids;
+	public $needPublicKeys;
+	public $needSecrets;
+
 	public function __construct ($ids, $needPublicKeys = false, $needSecrets = true) {
 		$this->ids = $ids;
 		$this->needPublicKeys = $needPublicKeys;
