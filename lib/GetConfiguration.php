97cb69793127585b5edfb41308e5f77d9beb0af1
Sébastien Santoro <dereckson@espace-win.org>
2015-11-19 06:02:23 +0000

    getconfig: command to get a configuration file
    Summary:
    Currently, getcredentials allow to get one setting. If several
    credentials are needed, we need to do several SSH connections
    to get each of them.
    
    We provide instead a `getconfig` script accepting a template in
    stdin or a file name to a file located in rOPS' config folder as
    parameter, and printing to stdout the configuration file after
    template variable substitution.
    
    Fixes T587.
    
    **Example:**
    
        bin/config forum/forum.yml
    
    **Template syntax:**
    
    | %%`command`%% | Runs the specified command and output the result |
    
    Test Plan: `ssh zr@ysul.nasqueron.org getconfig forum/forum.yml`
    
    Reviewers: dereckson
    
    Maniphest Tasks: T587
    
    Differential Revision: http://devcentral.nasqueron.org/D77

---
+++ lib/GetConfiguration.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Zemke-Rhyne
+ * Support tools for a Docker <--> Phabricator bridge
+ *
+ * (c) Sébastien Santoro aka Dereckson, 2015
+ * Released under BSD license.
+ */
+
+/**
+ * Class GetConfiguration
+ *
+ * Allows to get configuration from a template
+ */
+class GetConfiguration {
+	///
+	/// Properties
+	///
+
+	/**
+	 * The configuration template content
+	 * @var string
+	 */
+	public $template;
+
+	///
+	/// Templating methods
+	///
+
+	/**
+	 * Prints configuration from specified configuration template
+	 * replacing expressions by relevant variables and credentials.
+	 *
+	 * @return string the configuration
+	 */
+    public function printConfiguration () {
+		echo static::substituteTemplateVariables($this->template);
+    }
+
+	/**
+	 * Substitutes template variables in the specified text
+	 *
+	 * @param string $text The template text
+	 * @return string The substitued text
+	 */
+	public static function substituteTemplateVariables ($text) {
+		$text = static::substituteExec($text);
+		return $text;
+	}
+
+	/**
+	 * Substitutes %%`command`%% by the output of the command.
+	 *
+	 * @param string $text The template text
+	 * @return string The substitued text
+	 */
+	public static function substituteExec ($text) {
+		return preg_replace_callback(
+			'/%%`(.*)`%%/',
+			function ($matches) {
+				return trim(`$matches[1]`);
+			},
+			$text
+		);
+	}
+
+	///
+	/// Script procedural code
+	///
+
+	/**
+	 *  Runs the script
+	 */
+	public static function run ($file) {
+        $instance = new self;
+        $instance->template = file_get_contents($file);
+		$instance->printConfiguration();
+	}
+}
---
+++ tests/GetConfigurationTest.php
@@ -0,0 +1,42 @@
+<?php
+
+require '../lib/GetConfiguration.php';
+
+class GetConfigurationTest extends PHPUnit_Framework_TestCase {
+
+	public function testPrintConfiguration () {
+		$this->expectOutputString('foo');
+		$instance = new GetConfiguration();
+		$instance->template = 'foo';
+		$instance->printConfiguration();
+	}
+
+	public function testSubstituteTemplateVariables () {
+		//Empty string
+		$this->assertEquals('', GetConfiguration::substituteTemplateVariables(''));
+
+		//Equivalent to an empty string
+		$this->assertEquals('', GetConfiguration::substituteTemplateVariables(null));
+		$this->assertEquals('', GetConfiguration::substituteTemplateVariables(false));
+
+		//Nothing to substitute
+		$this->assertEquals('foo', GetConfiguration::substituteTemplateVariables('foo'));
+	}
+
+	/**
+	 * @requires OS Linux|BSD|CYGWIN|Darwin
+	 */
+	public function testSubstituteExec () {
+		$this->assertEquals('', GetConfiguration::substituteExec(''));
+
+		$template = '%%`echo -n ""`%%';
+		$this->assertEquals('', GetConfiguration::substituteExec($template));
+
+		$template = '%%`echo ""`%%';
+		$this->assertEquals('', GetConfiguration::substituteExec($template));
+
+		$template = 'File is %%`ls GetConfigurationTest.php`%%.';
+		$this->assertEquals('File is GetConfigurationTest.php.', GetConfiguration::substituteExec($template));
+	}
+
+}
